a925ce74923fcc7b46012737a687ec25c400e3a8
DianaSmertina <am.dianasmertina@gmail.com>
2023-08-30 15:24:28 +0300

    feat: add checkboxes for filter

--- app/src/components/toolbar/toolbar.tsx
+++ app/src/components/toolbar/toolbar.tsx
@@ -1,11 +1,36 @@
-import { Col } from "react-bootstrap";
+import { Col, InputGroup, Row } from "react-bootstrap";
+import { useGetAllGamesQuery } from "../../redux/freeToPlayApi";
 
 function Toolbar() {
+    const { data } = useGetAllGamesQuery();
+    const genres = new Set(data?.map((el) => el.genre));
+    const platforms = new Set(data?.map((el) => el.platform));
+
+    const addFilters = (options: Set<string>) => {
+        return Array.from(options).map((el) => (
+            <InputGroup key={el}>
+                <InputGroup.Checkbox value={el} />
+                <InputGroup.Text style={{ width: "70%" }}>{el}</InputGroup.Text>
+            </InputGroup>
+        ));
+    };
+
     return (
         <Col xs={3} className="border">
-            <div>Toolbar</div>
+            {data && (
+                <Row className="flex-column g-2 justify-content-center">
+                    <Col>
+                        <div className="h2">Genres</div>
+                        {addFilters(genres)}
+                    </Col>
+                    <Col>
+                        <div className="h2">Platforms</div>
+                        {addFilters(platforms)}
+                    </Col>
+                </Row>
+            )}
         </Col>
-    )
+    );
 }
 
 export default Toolbar;
