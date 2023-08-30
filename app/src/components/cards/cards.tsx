de97ccfaa93a2de8b92eda9353d07cc1a95ad1ee
DianaSmertina <am.dianasmertina@gmail.com>
2023-08-30 11:16:53 +0300

    feat: add pagination

--- app/src/components/cards/cards.tsx
+++ app/src/components/cards/cards.tsx
@@ -1,10 +1,27 @@
-import { Col, Row } from "react-bootstrap";
+import { Col, Row, Pagination } from "react-bootstrap";
 import { useGetAllGamesQuery } from "../../redux/freeToPlayApi";
 import Loading from "../loading/loading";
 import CardTemplate from "./cardTemplate";
+import { useEffect, useState } from "react";
 
 function Cards() {
     const { data, isError, isFetching } = useGetAllGamesQuery();
+    const [page, setPage] = useState(1);
+    const CARDS_ON_PAGE = 30;
+
+    const addPagination = () => {
+        const pageAmount = Math.ceil((data?.length || 0) / CARDS_ON_PAGE);
+        const paginationItems = new Array(pageAmount).fill("").map((_, i) => (
+            <Pagination.Item key={i + 1} active={i + 1 === page} onClick={() => setPage(i + 1)}>
+                {i + 1}
+            </Pagination.Item>
+        ));
+        return paginationItems;
+    };
+
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [page]);
 
     return (
         <Col xs={9} className="border">
@@ -15,7 +32,14 @@ function Cards() {
                     !isError &&
                     data &&
                     data.length > 0 &&
-                    data.map((el) => <CardTemplate key={el.id} data={el} />)}
+                    data
+                        .slice((page - 1) * CARDS_ON_PAGE, page * CARDS_ON_PAGE)
+                        .map((el) => <CardTemplate key={el.id} data={el} />)}
+                <Pagination>
+                    <Pagination.First />
+                    {addPagination()}
+                    <Pagination.Last />
+                </Pagination>
             </Row>
         </Col>
     );
--- app/src/pages/gamePage/gamePage.tsx
+++ app/src/pages/gamePage/gamePage.tsx
@@ -1,17 +1,21 @@
 import { useParams } from "react-router-dom";
 import { useGetGameByIdQuery } from "../../redux/freeToPlayApi";
 import Loading from "../../components/loading/loading";
-import { Row } from "react-bootstrap";
+import { Button, Row } from "react-bootstrap";
 import Game from "../../components/game/game";
 import { Link } from "react-router-dom";
+
+import styles from "./gamePage.module.scss";
 
 function GamePage() {
     const { gameId } = useParams();
     const { data, isError, isFetching } = useGetGameByIdQuery(gameId || "");
 
     return (
-        <Row className="justify-content-center">
-            <Link to="/">Back on main page</Link>
+        <Row className="justify-content-center flex-column py-1">
+            <Link to="/" className={styles.link}>
+                <Button>Back</Button>
+            </Link>
             {isFetching && <Loading />}
             {isError && <p>Error occurred</p>}
             {!isFetching && !isError && data && <Game data={data} />}
