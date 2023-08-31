ef94fcb70e628b013cf1d69c6c4dfd2a1e266552
DianaSmertina <am.dianasmertina@gmail.com>
2023-08-31 16:03:34 +0300

    feat: add 3 retries query if first returns error

--- app/src/redux/freeToPlayApi.tsx
+++ app/src/redux/freeToPlayApi.tsx
@@ -1,20 +1,24 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { createApi, fetchBaseQuery, retry } from "@reduxjs/toolkit/query/react";
 import { IGame, IGameExtended, IParamsGamesList } from "../types/types";
 const key = import.meta.env.VITE_API_KEY;
 
+const staggeredBaseQuery = retry(fetchBaseQuery({
+    baseUrl: "https://free-to-play-games-database.p.rapidapi.com/api",
+    prepareHeaders: (headers) => {
+        headers.set("X-RapidAPI-Key", key);
+        headers.set(
+            "X-RapidAPI-Host",
+            "free-to-play-games-database.p.rapidapi.com"
+        );
+        return headers;
+    },
+}), {
+    maxRetries: 3,
+})
+
 export const freeToPlayApi = createApi({
     reducerPath: "freeToPlayApi",
-    baseQuery: fetchBaseQuery({
-        baseUrl: "https://free-to-play-games-database.p.rapidapi.com/api",
-        prepareHeaders: (headers) => {
-            headers.set("X-RapidAPI-Key", key);
-            headers.set(
-                "X-RapidAPI-Host",
-                "free-to-play-games-database.p.rapidapi.com"
-            );
-            return headers;
-        },
-    }),
+    baseQuery: staggeredBaseQuery,
     endpoints: (builder) => ({
         getAllGames: builder.query<Array<IGame>, IParamsGamesList>({
             query: (params) => {
